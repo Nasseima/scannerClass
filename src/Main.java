0f8679b4192362d7b6559eb499e0594752e3f27d
Nasseima <lindsaynasseima@gmail.com>
2024-06-27 08:54:21 -0400

    testing out scanner class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello " + name + ", it's nice to meet you!");
+    }
+}
